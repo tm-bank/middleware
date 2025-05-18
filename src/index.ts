504a780d41c89c21f0b408c8076720b78a5e1373
languint <longuint@proton.me>
2025-05-18 13:30:08 -0500

    Update policies

--- src/index.ts
+++ src/index.ts
@@ -45,7 +45,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: false,
+      secure: true,
       sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     },
